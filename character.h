d9a114a199872f84372d3b656a368815195f9c63
Lucas Hiroshi Hayashida <hiroshi.hayashida@gmail.com>
2014-06-07 17:36:22 -0300

    Header for character.c.

---
+++ character.h
@@ -0,0 +1,12 @@
+#ifndef H_CHARACTER
+#define H_CHARACTER
+
+#include <stdio.h>
+#include <stdlib.h>
+
+extern double characters[126];
+
+void init_array();
+void count_characters(char *file_name); 
+
+#endif /*H_CHARACTER*/
