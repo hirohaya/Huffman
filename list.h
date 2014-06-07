f4b6e015990480a6a53e75eaa5b1b6888ad8f8c9
Lucas Hiroshi Hayashida <hiroshi.hayashida@gmail.com>
2014-06-07 18:23:52 -0300

    Header for list.h. Contains the nodes and queue data structures.

---
+++ list.h
@@ -0,0 +1,32 @@
+#ifndef H_LIST
+#define H_LIST
+
+
+typedef struct node{
+    char symbol;
+    int freq;
+    int isRight; /*1 significa que esta a direita, 0 esta a esquerda*/
+    struct node *father;
+    struct node *next;
+} Node;
+
+
+
+
+typedef struct queue{
+
+	Node *start;
+	Node *end;
+
+} Queue;
+
+
+
+
+extern Queue p_queue; /*Lista de prioridade*/
+
+	
+void init_queue();
+void insert_node(Node *node);
+
+#endif /*H_LIST*/
