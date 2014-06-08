3ade8d54b55df6225f8fa0e5ca6aa398dedb3543
Lucas Hiroshi Hayashida <hiroshi.hayashida@gmail.com>
2014-06-08 00:56:52 -0300

    Implemented tests for all project's functions.

--- main.c
+++ main.c
@@ -1,17 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "tree.h"
 #include "character.h"
+#include "list.h"
+#include "tree.h"
+
+
 
 double characters[126];
+Queue *p_queue;
+
 
 int main(int argv, char **argc){
 
+  int i;
 
+  	init_queue();
 	init_array();	
 	count_characters(argc[1]);
 
+ 	for(i = 0; i < 126; i++)
+		if((characters[i] > 0 && i >= 31 && i <= 125) || i == 9 || i == 8)
+			/*Lisp aproves*/
+			insert_node((create_node((i+1), characters[i])));
+	/*Teste das funcoes de filas*/
+	print_queue();
+	printf("Temos %d caracteres diferentes.\n", get_size());
+	create_leaves();
 
        return 0;
 }
+
