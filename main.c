cc9bb9283391e0d6a99eaba43b506c60f30f9593
Lucas Hiroshi Hayashida <hiroshi.hayashida@gmail.com>
2014-06-08 06:58:01 -0300

    There are no reference to any global variables.

--- main.c
+++ main.c
@@ -7,27 +7,20 @@
 
 
 
-double characters[126];
-Queue *p_queue;
-
-
 int main(int argv, char **argc){
 
-  int i;
 
   	init_queue();
 	init_array();	
-	count_characters(argc[1]);
-
- 	for(i = 0; i < 126; i++)
-		if((characters[i] > 0 && i >= 31 && i <= 125) || i == 9 || i == 8)
-			/*Lisp aproves*/
-			insert_node((create_node((i+1), characters[i])));
-	/*Teste das funcoes de filas*/
-	print_queue();
-	printf("Temos %d caracteres diferentes.\n", get_size());
+	count_characters(argc[2]);
+	create_queue();
+ 	/*Teste das funcoes de filas*/
+	/*print_queue();*/
 	create_leaves();
-
-       return 0;
+	init_tree();
+	build_tree();
+	print_tree();
+	compact_file(argc[2], argc[4]);	
+	return 0;
 }
 
