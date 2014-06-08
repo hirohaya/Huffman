77f515042b4ef7a2a17b5617bb97ea029f1d8c4d
Lucas Hiroshi Hayashida <hiroshi.hayashida@gmail.com>
2014-06-08 07:00:01 -0300

    Created function to initialize, build and print the binary tree. Created functions to write the codification in a new file.

--- tree.c
+++ tree.c
@@ -1,2 +1,104 @@
 #include "tree.h"
 
+
+
+Queue *p_queue;
+Node **leaves;
+Node **btree;
+int nleaves;
+
+void init_tree(){
+
+	nleaves = get_size();
+	btree = malloc((2 * nleaves - 1)*sizeof(Node));
+}
+
+
+
+void build_tree(){
+
+  Node *p;
+  Node *q;
+  Node *new;
+  int i;
+
+  	i = 0;
+
+	while(get_size() > 1){
+
+		p = p_queue->start;
+		rem_node();
+		q = p_queue->start;
+		rem_node();
+
+		new = create_node(0, (p->freq + q->freq));
+
+		p->father = new;
+		p->isLeft = 1;
+		
+		q->father = new;
+		q->isLeft = 0;
+
+		insert_node(new);
+		btree[i] = new;
+		
+		i++;
+	}
+}
+
+void print_tree(){
+
+  Node *p;
+  int i;
+  	
+  	for(i = 0; i < nleaves; i++){
+  		for(p  = leaves[i]; p != NULL; p = p->father){
+			printf("%f\n", p->freq);
+		}
+		puts("");
+	}
+}
+
+
+
+void print_code(Node *leaf, FILE *oFile){
+
+	if(leaf->father->father != NULL)
+		print_code(leaf->father, oFile);
+	fprintf(oFile, "%d", leaf->isLeft);
+}
+
+
+
+void compact_file(char *iname, char *oname){
+
+  FILE *iFile;
+  FILE *oFile;
+  int i;
+  char c;
+
+  	iFile = fopen(iname, "r");
+	oFile = fopen(oname, "w");
+	/*Formatacao da impressao da codificao: "[valor inteiro do caractere na tabela ASCII]:[Codificacao do caractere]*/
+	fprintf(oFile, "%d\n", nleaves);
+	/*Imprime no arquivo de saida (compactado) a codificacao de cada caractere utilizado*/
+	for(i = 0; i < nleaves; i++){
+		fprintf(oFile, "%d:", leaves[i]->symbol);
+		print_code(leaves[i], oFile);
+		fprintf(oFile, "\n");
+	}
+
+/*	while(!feof(iFile)){
+		c = fgetc(iFile);
+		for(i = 0; i < nleaves-1; i++)
+			if(leaves[i]->symbol == c)
+				break;
+		print_code(leaves[i], oFile);
+	}*/
+
+
+	fprintf(oFile, "\n");
+	fclose(iFile);
+	fclose(oFile);
+}
+
--- tree.h
+++ tree.h
@@ -3,9 +3,20 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#include "list.h"
 
 
 
+extern Node **btree;
+extern int nleaves;
 
-#endif
+
+void init_tree();
+void build_tree();
+void print_tree();
+void print_code(Node *leaf, FILE *oFile);
+void compact_file(char *iname, char *oname);
+#endif /*H_TREE*/
 
